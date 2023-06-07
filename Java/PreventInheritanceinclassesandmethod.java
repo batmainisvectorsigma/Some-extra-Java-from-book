1d74bf40f043e39c77d9a2021864e1934a2bd18b
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-06-07 23:28:06 +0545

    Update PreventInheritanceinclassesandmethod.java

--- Java/PreventInheritanceinclassesandmethod.java
+++ Java/PreventInheritanceinclassesandmethod.java
@@ -1,1 +1,16 @@
+public final class PreventInheritanceinclassesandmethod{
 
+}
+public class Main extends PreventInheritanceinclassesandmethod{
+  //here i am getting some error cause you cannot extends the final classes
+}
+public final class PreventInheritanceinclassesandmethod{
+   public void run(){
+     
+   }
+}
+//once the class is made final all the method automtically becomes final and no subclass can override that
+//method
+//final fields cannot be changed after the object has been constructed
+
+//If I want to prevent others from subclassing the executive class Simply declare the class using the final modifier
