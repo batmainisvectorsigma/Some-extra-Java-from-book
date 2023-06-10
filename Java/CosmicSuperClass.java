f64743819a0cfba46d156e6aa965f4f492d8ac16
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-06-10 09:51:41 +0545

    Update CosmicSuperClass.java

--- Java/CosmicSuperClass.java
+++ Java/CosmicSuperClass.java
@@ -1,4 +1,4 @@
-//note that if you are trying are trying to use this program please use make your program as Main.java
+//note that if you are trying to use this program please use make your program as Main.java
 
 class Main extends Object{
     String name,address;
