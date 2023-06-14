1adc4aba2d5504e6c1c0db2b77938e3242a6ba58
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-06-14 21:32:45 +0545

    Update programone.java

--- Java/Generics/programone.java
+++ Java/Generics/programone.java
@@ -1,1 +1,35 @@
+//Here T means of any type
+public class Printer<T>{
+ T thingToPrint;
+  public Printer(T thingToPrint){
+    this.thingToPrint=thingToPrint;
+  }
+  public void print(){
+    System.out.println(thingToPrint);
+  }
+}
+public class GenericsExamnple{
+  public static void main(String args[]){
+    Printer<Integer> printer=new Printer<>(23);
+    printer.print();
+  }
+}
 
+//The rest of the program is same over here
+public class Printer<T>{
+ T thingToPrint;
+  public Printer(T thingToPrint){
+    this.thingToPrint=thingToPrint;
+  }
+  public void print(){
+    System.out.println(thingToPrint);
+  }
+}
+public class GenericsExamnple{
+  public static void main(String args[]){
+    Printer<Integer> printer=new Printer<>(23);
+    printer.print();      //output 23
+    Printer<Double> doublePrinter=new Printer<>(23.5);
+    doublePrinter.print();  //output 23.5
+  }
+}
