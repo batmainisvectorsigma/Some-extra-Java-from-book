5a2afc354f9dcd3ce2650db1312e125697c798f3
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-09-13 00:07:48 -0700

    Create EnumTestIfElseSwitch.java

---
+++ Java/Enumeration Enum/EnumTestIfElseSwitch.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+enum Day{
+    SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY;
+}
+public class EnumTestIfElseSwitch {
+    public static void main(String args[]){
+        System.out.println("You are geeting holday only on Satuday \n Enter today's Day");
+        Scanner input=new Scanner(System.in);
+        String day=input.nextLine().toUpperCase();
+        String d1=(Day.SATURDAY).toString();
+        if(day.equals((Day.SATURDAY).toString())){
+            System.out.println("You got the holiday");
+            System.out.println(d1);
+            System.out.println(Day.SUNDAY);
+        }
+        else{
+            System.out.println("Wow! Hardworking programmer lets's work");
+        }
+    }
+}
