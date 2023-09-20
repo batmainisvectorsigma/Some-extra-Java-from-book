da8d2ad048d01cf81cf2cb21065b1e88880578ac
whereyoucan'tfind <74447376+vectorsigmaisbatman@users.noreply.github.com>
2023-09-21 04:25:39 +0545

    Create MethodReferences.java

---
+++ Java/Lambda expression/MethodReferences.java
@@ -0,0 +1,17 @@
+//EXMAPLE NO : 1
+//Showing the difference between using lambda expression and method references
+import java.util.List;
+import java.util.Arrays;
+
+public class Main{
+	public static void main(String args[]){
+		List<String> names=Arrays.asList("Alice","Bob","Charlie");
+		//Using Lambda:
+		System.out.println("Using Lambda");
+		names.forEach(name -> System.out.println(name));
+		//Using Method References:
+		System.out.println("\n Using method Reference:");
+		names.forEach(System.out::println);
+	}
+}
+
