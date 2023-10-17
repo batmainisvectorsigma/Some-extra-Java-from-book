26a12617c4bf5b22fab94675352d0b38011f2397
whereyoucan'tfind <74447376+batmainisvectorsigma@users.noreply.github.com>
2023-10-17 17:59:14 +0545

    Create DataType.java

---
+++ Java/Fundamental/DataType.java
@@ -0,0 +1,37 @@
+//------------------------------------COMMENTS
+// this is called as single line comment where
+/*
+This is called multiline comments
+*/
+//-----------------------------------PRIMITIVE DATATYPES
+public class Main{
+    public static void main(String args[]){
+        int a=12;
+        float b=5.9f;
+        double c=9.25;
+        short d=32;
+        long e=9223372036854775807L;
+        byte f=125;
+        boolean g=true;
+        char h='u';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
+/*
+OUTPUT
+12
+5.9
+9.25
+32
+9223372036854775807
+125
+true
+u
+*/
